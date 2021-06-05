5af895e1e4ebaa9b96092db8061d4d0c436368ea
Dinesh0459 <71681299+Dinesh0459@users.noreply.github.com>
2021-06-05 16:27:54 +0530

    Update newlo.js

--- newlo.js
+++ newlo.js
@@ -3,10 +3,10 @@ function validate() {
     var password = document.getElementById("password").value;
     if (username == "Dinesh32" && password == "Srm3000") {
          alert("LOGGED IN SUCCESFULLY");
-         window.location.href="https://colab.research.google.com/drive/13Ktsrx164eQHfDmYLyMCoI-Kq0gC5Kg1";
+         window.location.href="http://localhost:8501";
         try {
             // window.location.href = "./login.html";
-            setTimeout(function(){document.location.href = "https://colab.research.google.com/drive/13Ktsrx164eQHfDmYLyMCoI-Kq0gC5Kg1"},500);
+            setTimeout(function(){document.location.href = "http://localhost:8501"},500);
 
         } catch(err) {
             console.log(err);
